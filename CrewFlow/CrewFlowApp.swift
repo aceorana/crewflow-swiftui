1802a61524260752978b8da6cf05ea3f6fe51e79
user278387 <user278387@#242.110>
2025-12-03 12:05:46 -0800

    Initial SwiftUI project setup: CrewFlow app structure

---
+++ CrewFlow/CrewFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrewFlowApp.swift
+//  CrewFlow
+//
+//  Created by user278387 on 12/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CrewFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CrewFlowTests/CrewFlowTests.swift
@@ -0,0 +1,16 @@
+//
+//  CrewFlowTests.swift
+//  CrewFlowTests
+//
+//  Created by user278387 on 12/3/25.
+//
+
+import Testing
+
+struct CrewFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
